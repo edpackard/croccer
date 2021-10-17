56d78ec9163e21b9cffe844ce9d02b86016c5fd6
edpackard <edpackard81@gmail.com>
2021-10-17 13:46:52 +0100

    add game loop

--- croccer.js
+++ croccer.js
@@ -9,6 +9,18 @@ function init() {
   canvas = document.getElementById("canvas");
   context = canvas.getContext("2d");
   background();
+  window.requestAnimationFrame(gameLoop);
+}
+
+function gameLoop(timeStamp) {
+  draw();
+  window.requestAnimationFrame(gameLoop);
+}
+
+function draw() {
+  let randomColor = Math.random() > 0.5 ? "#ff8080" : "#0099b0";
+  context.fillStyle = randomColor;
+  context.fillRect(100, 50, 200, 175);
 }
 
 function background() {
