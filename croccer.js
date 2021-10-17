2a10b297dce6400d7d58194368cd29a3dc7205e8
edpackard <edpackard81@gmail.com>
2021-10-17 13:29:34 +0100

    add background color

--- croccer.js
+++ croccer.js
@@ -8,11 +8,11 @@ window.onload = init;
 function init() {
   canvas = document.getElementById("canvas");
   context = canvas.getContext("2d");
-  draw();
+  background();
 }
 
-function draw() {
-  let randomColor = Math.random() > 0.5 ? "#ff8080" : "#0099b0";
-  context.fillStyle = randomColor;
-  context.fillRect(100, 50, 200, 175);
+function background() {
+  let backgroundColor = "#0099d0";
+  context.fillStyle = backgroundColor;
+  context.fillRect(0, 0, 750, 400);
 }
