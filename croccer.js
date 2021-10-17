449674495d653c134a6cb8117a56fc796635f367
edpackard <edpackard81@gmail.com>
2021-10-17 14:01:57 +0100

    add update function for movement

--- croccer.js
+++ croccer.js
@@ -12,31 +12,27 @@ function init() {
   window.requestAnimationFrame(gameLoop);
 }
 
-let secondsPassed;
-let oldTimeStamp;
-let fps;
+let rectX = 0;
+let rectY = 0;
 
 function gameLoop(timeStamp) {
-  secondsPassed = (timeStamp - oldTimeStamp) / 1000;
-  oldTimeStamp = timeStamp;
-  fps = Math.round(1 / secondsPassed);
-  context.fillStyle = "white";
-  context.fillRect(0, 0, 200, 100);
-  context.font = "25px Arial";
-  context.fillStyle = "black";
-  context.fillText("FPS: " + fps, 10, 30);
+  update();
   draw();
   window.requestAnimationFrame(gameLoop);
 }
 
+function update() {
+  rectX += 1;
+  rectY += 1;
+}
+
 function draw() {
-  let randomColor = Math.random() > 0.5 ? "#ff8080" : "#0099b0";
-  context.fillStyle = randomColor;
-  context.fillRect(100, 50, 200, 175);
+  context.strokeStyle = "#ff8080";
+  context.strokeRect(rectX, rectY, 50, 50);
 }
 
 function background() {
   let backgroundColor = "#0099d0";
   context.fillStyle = backgroundColor;
-  context.fillRect(0, 0, 750, 400);
+  context.strokeRect(0, 0, 750, 400);
 }
